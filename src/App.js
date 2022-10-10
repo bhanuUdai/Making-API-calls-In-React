76956833c60210a603414018f3573adf170f70e0
bhanu <udaibhanu2602@gmail.com>
2022-10-11 00:32:13 +0530

    Converting into async await

--- src/App.js
+++ src/App.js
@@ -4,38 +4,18 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
-
   const [movies,setMovies]=useState([])
 
-  const fetchMoviesHandler=()=>
+  async function fetchMoviesHandler()   // making function async function
   {
-    fetch('https://swapi.dev/api/films')
-    .then((response)=>
-    {
-      return response.json()
-    })
-    .then((data)=>
-    {
-      console.log(data.results)
+    let res=await fetch('https://swapi.dev/api/films')  // the response from fetching server will store in res here 
+    try{
+      let fetchMovies=await res.json();  //.json() also async function, it will convert json fromat into readable js , but it is also a callback (i.e) promise therefore using await with it also
 
-      let transformedMovies=data.results.map((data)=>
+      let transformedMovies=fetchMovies.results.map((data)=>
       {
         return({
-          id: data.episode_id,
+          id: data.episode_id,   // here we are giving values to the id, in server id is not like id: 1, but episode_id:1, therefor we are assign that values to understand by movies.js component
           title: data.title,
           openingText: data.opening_crawl,
           releaseDate: data.release_date,
@@ -45,7 +25,37 @@ function App() {
 
       setMovies(transformedMovies)
 
-    })
+
+    }
+    catch (err)
+    {
+      console.log(err)
+    }
+
+
+    // fetch('https://swapi.dev/api/films')
+    // .then((response)=>
+    // {
+    //   return response.json()
+    // })
+    // .then((data)=>
+    // {
+    //   console.log(data.results)
+
+    //   let transformedMovies=data.results.map((data)=>
+    //   {
+    //     return({
+    //       id: data.episode_id,
+    //       title: data.title,
+    //       openingText: data.opening_crawl,
+    //       releaseDate: data.release_date,
+
+    //     })
+    //   })
+
+    //   setMovies(transformedMovies)
+
+    // })
   }
 
   return (
